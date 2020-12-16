de237d2a41bc549ebc1ebeb693d89cff9696525c
Łukasz Stępień <lukasz.stepien.programista@gmail.com>
2020-12-16 22:10:09 +0100

    Introducing the User class to the user management part of the code ver.2

---
+++ User.h
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <fstream>
+#include <sstream>
+#include <windows.h>
+
+using namespace std;
+
+class User
+{
+    int idOfUser, idOfLoggedUser;
+    string userName, password;
+    vector<User> users;
+
+
+public:
+    User(int=0, int=0, string="", string="");
+    ~User();
+
+    friend string loadLineOfText();
+
+    int getLoggedUserId();
+    void userRegistration();
+    int logging();
+    void passwordChange();
+    void enterTheUsersDetails();
+    void saveUsersToTxtFile();
+    void readUsersFromTxtFile();
+
+};
